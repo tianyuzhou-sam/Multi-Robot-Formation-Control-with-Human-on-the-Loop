ba13fee594921523c52ea5e22174ae6da316775e
tianyuzhou-sam <zhou1043@purdue.edu>
2023-11-15 14:47:41 -0500

    Debug

--- experiment/ModelPredictiveControl.py
+++ experiment/ModelPredictiveControl.py
@@ -10,6 +10,7 @@ from JackalSys import JackalSys
 from OptimalControlProblem import OptimalControlProblem
 import json
 import transforms3d
+import csv
 
 import asyncio
 import xml.etree.ElementTree as ET
@@ -24,15 +25,17 @@ from geometry_msgs.msg import Twist
 class ModelPredictiveControl:
     configDict: dict  # a dictionary for parameters
 
-    def __init__(self, configDict: dict, buildFlag=True, config_file_name='config/config_jackal.json'):
+    def __init__(self, configDict: dict, buildFlag=True, targets=[[0,0]], saveFlag=False, config_file_name='config/config_jackal.json'):
         self.configDict = configDict
+        self.dt = self.configDict['dt']
+        self.saveFlag = saveFlag
 
         # initialize JackalSys
         self.MyJackalSys = JackalSys(configDict, buildFlag)
-        self.targets = [[0,0], [10,5], [10,10]]
-        target = self.targets[0]
+        self.targets = targets
+        self.offset = 0.1
         # initialize OptimalControlProblem
-        self.MyOC = OptimalControlProblem(configDict, self.MyJackalSys, buildFlag, target)
+        self.MyOC = OptimalControlProblem(configDict, self.MyJackalSys, buildFlag)
         
         # method
         try:
@@ -52,8 +55,8 @@ class ModelPredictiveControl:
         self.IP_server = self.config_data["QUALISYS"]["IP_MOCAP_SERVER"]
 
 
-        rospy.init_node('move_robot_node', anonymous=False)
-        self.pub_move = rospy.Publisher("/cmd_vel", Twist, queue_size=100)
+        # rospy.init_node('move_robot_node', anonymous=False)
+        # self.pub_move = rospy.Publisher("/cmd_vel", Twist, queue_size=100)
 
     def create_body_index(self, xml_string):
         """ Extract a name to index dictionary from 6-DOF settings xml """
@@ -68,7 +71,7 @@ class ModelPredictiveControl:
     def publish_vel(self):
         self.pub_move.publish(self.move)
 
-    async def run(self, iniState: np.array, iniInput: np.array, timeTotal: float):
+    async def run(self, iniState: np.array, timeTotal: float):
         # Connect to qtm
         connection = await qtm.connect(self.IP_server)
 
@@ -89,10 +92,11 @@ class ModelPredictiveControl:
         # the one we want to access
         wanted_body = self.config_data["QUALISYS"]["NAME_SINGLE_BODY"]
         
-        self.timeNow = 0.0  # [sec]
+        self.t_start = time.time()
+        self.timeNow = time.time() - self.t_start  # [sec]
         self.stateNow = copy.deepcopy(iniState)
-        self.inputNow = copy.deepcopy(iniInput)
         self.idx = 0
+        self.target = self.targets[0]
 
         # initialize trajectories for states, inputs, etc.
         self.timeTraj = np.array([self.timeNow], dtype=np.float64)
@@ -109,112 +113,58 @@ class ModelPredictiveControl:
         self.logTimeTraj = list()
         self.logStrTraj = list()
 
-        # target = self.targets[0]
-        # self.MyOC = OptimalControlProblem(configDict, self.MyJackalSys, buildFlag, target)
         # load function to run optimization once
         if self.methodStr == "MPC":
-            self.runOnce = lambda stateNow, timeNow: self._runOC(self.stateNow, self.timeNow)
+            self.runOnce = lambda stateNow, timeNow, target: self._runOC(self.stateNow, self.timeNow, self.target)
         else:
             raise Exception("Wrong method in configDict!")
 
         self.reached = 0
 
-        # while (timeNow <= timeTotal-self.MyOC.dt) and (reached < self.numTargets):
-
-        #     # solve
-        #     ipoptTime, returnStatus, successFlag, algoTime, print_str, uNow = self.runOnce(stateNow, timeNow)
-
-        #     # apply control and forward propagate dynamics
-        #     xNext = self.MyJackalSys.discDynFun(stateNow, uNow)
-
-        #     # update trajectory
-        #     stateNow = np.array(xNext).reshape((1,-1)).flatten()
-        #     xTraj = np.vstack((xTraj, stateNow))
-        #     if idx <= 0.5:
-        #         uTraj[idx, :] = uNow
-        #         algTimeTraj[idx] = algoTime
-        #         ipoptTimeTraj[idx] = ipoptTime
-        #     else:
-        #         uTraj = np.vstack((uTraj, uNow))
-        #         algTimeTraj = np.append(algTimeTraj, algoTime)
-        #         ipoptTimeTraj = np.append(ipoptTimeTraj, ipoptTime)
-
-        #     if idx % 50 == 0:
-        #         print(print_str)
-        #     if not successFlag:
-        #         if idx % 50 != 0:
-        #             print(print_str)
-        #         print(returnStatus)
-        #         logTimeTraj.append(timeNow)
-        #         logStrTraj.append(returnStatus)
-
-        #     # update time
-        #     timeNow += self.MyOC.dt
-        #     idx += 1
-        #     timeTraj = np.append(timeTraj, timeNow)
-
-        #     # if ((stateNow[0]-self.targets[reached][0])**2+(stateNow[1]-self.targets[reached][1])**2 <= self.offset**2):
-        #     #     reached += 1
-        #     #     if reached < self.numTargets:
-        #     #         target = self.targets[reached]
-        #     #         self.MyOC = OptimalControlProblem(configDict, self.MyJackalSys, buildFlag, target)
-
-        # print(print_str)
-        # result = {"timeTraj": timeTraj,
-        #           "xTraj": xTraj,
-        #           "uTraj": uTraj,
-        #           "ipoptTimeTraj": ipoptTimeTraj,
-        #           "logTimeTraj": logTimeTraj,
-        #           "logStrTraj": logStrTraj}
-
-        # return result
-
         def on_packet(packet):
             # Get the 6-DOF data
             bodies = packet.get_6d()[1]
-            t_now = 0
-            if (t_now < 10):
-                # Extract one specific body
-                t_now = time.time()
-                wanted_index = body_index[wanted_body]
-                position, rotation = bodies[wanted_index]
-                # You can use position and rotation here. Notice that the unit for position is mm!
-                # print(wanted_body)
+            # Extract one specific body
+            wanted_index = body_index[wanted_body]
+            position, rotation = bodies[wanted_index]
+            # You can use position and rotation here. Notice that the unit for position is mm!
+            # print(wanted_body)
 
-                # rotation.matrix is a tuple with 9 elements.
-                rotation_np = np.asarray(rotation.matrix, dtype=float).reshape(3, 3)
+            # rotation.matrix is a tuple with 9 elements.
+            rotation_np = np.asarray(rotation.matrix, dtype=float).reshape(3, 3)
 
-                # send 6-DOF data via TCP
-                # concatenate the position and rotation matrix vertically
-                # msg = np.asarray((position.x/1000.0, position.y/1000.0, position.z/1000.0) + rotation.matrix, dtype=float).tobytes()
+            # send 6-DOF data via TCP
+            # concatenate the position and rotation matrix vertically
+            # msg = np.asarray((position.x/1000.0, position.y/1000.0, position.z/1000.0) + rotation.matrix, dtype=float).tobytes()
 
-                quat = transforms3d.quaternions.mat2quat(rotation_np)
-                # print("quat")
-                # print(quat)
+            quat = transforms3d.quaternions.mat2quat(rotation_np)
+            # print("quat")
+            # print(quat)
 
-                data = np.array([position.x/1000.0, position.y/1000.0, position.z/1000.0, quat[0], quat[1], quat[2], quat[3], t_now], dtype=float)
+            data = np.array([position.x/1000.0, position.y/1000.0, position.z/1000.0, quat[0], quat[1], quat[2], quat[3], self.timeNow], dtype=float)
 
-                # # for debugging
-                # print("rotation matrix in array")
-                # print(rotation.matrix)
-                # print("rotation matrix in matrix")
-                # print(rotation_np)
+            # # for debugging
+            # print("rotation matrix in array")
+            # print(rotation.matrix)
+            # print("rotation matrix in matrix")
+            # print(rotation_np)
 
-                roll_now, pitch_now, yaw_now = transforms3d.euler.quat2euler(quat, axes='sxyz')
-                # print("yaw")
-                print(math.degrees(yaw_now))
-                # print("pitch")
-                # print(math.degrees(pitch_now))
-                # print("roll")
-                # print(math.degrees(roll_now))
+            roll_now, pitch_now, yaw_now = transforms3d.euler.quat2euler(quat, axes='sxyz')
+            yaw_now = -yaw_now
+            
+            # print(position)
+            # print("yaw")
+            # print(yaw_now)
+            # print(math.degrees(yaw_now))
 
+            if ((position[0] - self.target[0])**2 + (position[1] - self.target[1])**2 >= self.offset**2):
+                
                 # solve
-                ipoptTime, returnStatus, successFlag, algoTime, print_str, uNow = self.runOnce(self.stateNow, self.timeNow)
+                ipoptTime, returnStatus, successFlag, algoTime, print_str, uNow = self.runOnce(self.stateNow, self.timeNow, self.target)
 
                 # apply control and forward propagate dynamics
-                xNext = self.MyJackalSys.discDynFun(self.stateNow, uNow)
-                xNext[0] = position[0]
-                xNext[1] = position[1]
+                # xNext = self.MyJackalSys.discDynFun(self.stateNow, uNow)
+                xNext = [position[0], position[1], yaw_now]
 
                 # update trajectory
                 self.stateNow = np.array(xNext).reshape((1,-1)).flatten()
@@ -231,14 +181,14 @@ class ModelPredictiveControl:
                 if self.idx % 50 == 0:
                     print(print_str)
                 if not successFlag:
-                    if idx % 50 != 0:
+                    if self.idx % 50 != 0:
                         print(print_str)
                     print(returnStatus)
                     self.logTimeTraj.append(self.timeNow)
                     self.logStrTraj.append(returnStatus)
 
                 # update time
-                self.timeNow += self.MyOC.dt
+                self.timeNow = time.time() - self.t_start
                 self.idx += 1
                 self.timeTraj = np.append(self.timeTraj, self.timeNow)
 
@@ -248,32 +198,40 @@ class ModelPredictiveControl:
                 #         target = self.targets[reached]
                 #         self.MyOC = OptimalControlProblem(configDict, self.MyJackalSys, buildFlag, target)
 
-                print(print_str)
+                # print(print_str)
+                
+                move =Twist()
+                move.linear.x = uNow[0]
+                move.angular.z = uNow[1]
+                self.pub_move.publish(move) 
+                
+
+                # time.sleep(self.dt)  
+            
+            else:
+                # Finish up
                 result = {"timeTraj": self.timeTraj,
                             "xTraj": self.xTraj,
                             "uTraj": self.uTraj,
                             "ipoptTimeTraj": self.ipoptTimeTraj,
                             "logTimeTraj": self.logTimeTraj,
                             "logStrTraj": self.logStrTraj}
-                # print(result)
 
-                move =Twist()
-                move.linear.x = uNow[0]
-                move.angular.z = uNow[1]
-                self.pub_move.publish(move)   
-            
-            else:
-                # error
-                raise Exception("There is no such a rigid body!")
+                if self.saveFlag:
+                    with open('controlData.csv', 'w') as file:
+                        writer = csv.writer(file)
+                        for item, value in result.items():
+                            writer.writerow([item, value])
+                raise Exception("Done")
 
         # Start streaming frames
         # Make sure the component matches with the data fetch function, for example: packet.get_6d() with "6d"
         # Reference: https://qualisys.github.io/qualisys_python_sdk/index.html
         await connection.stream_frames(components=["6d"], on_packet=on_packet)
 
-    def _runOC(self, stateNow, timeNow):
+    def _runOC(self, stateNow, timeNow, target):
         t0 = time.time()
-        xTrajNow, uTrajNow, timeTrajNow, ipoptTime, returnStatus, successFlag = self.MyOC.solve(stateNow, timeNow)
+        xTrajNow, uTrajNow, timeTrajNow, ipoptTime, returnStatus, successFlag = self.MyOC.solve(stateNow, timeNow, target)
         t1 = time.time()
         algoTime = t1 - t0
         print_str = "Sim time [sec]: " + str(round(timeNow, 1)) + "   Comp. time [sec]: " + str(round(algoTime, 3))
@@ -347,18 +305,19 @@ if __name__ == '__main__':
     config_file_name = 'config/config_jackal.json'
 
     buildFlag = True
-    # buildFlag = False
+    saveFlag = False
 
     
 
     x0 = np.array([0, 0, 0])
     u0 = np.array([0, 0])
     T = 20
+    targets = [[0,0], [10,5], [10,10]]
 
     # initialize MPC
-    MyMPC = ModelPredictiveControl(configDict, buildFlag, config_file_name)
+    MyMPC = ModelPredictiveControl(configDict, buildFlag, targets, saveFlag, config_file_name)
 
     # Run our asynchronous main function forever
-    asyncio.ensure_future(MyMPC.run(x0, u0, T))
+    asyncio.ensure_future(MyMPC.run(x0, T))
     asyncio.get_event_loop().run_forever()
 
